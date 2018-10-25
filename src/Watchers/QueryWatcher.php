b21a616e62bc0a9a099d100e9cd781e02554bb9d
Mark O'Keeffe <mark.okeeffe@digistorm.com.au>
2018-10-25 16:57:16 +1000

    Removed obsolete file

--- src/Watchers/QueryWatcher.php
+++ src/Watchers/QueryWatcher.php
@@ -2,7 +2,6 @@
 
 namespace Laravel\Telescope\Watchers;
 
-use Laravel\Telescope\StackTrace;
 use Laravel\Telescope\Telescope;
 use Laravel\Telescope\IncomingEntry;
 use Illuminate\Database\Events\QueryExecuted;
